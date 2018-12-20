9d71f61bcfcd12c8c10c16ca28e24caf18af6a92
jamiahuswalton <jamiahuswalton@gmail.com>
2018-12-20 10:47:12 -0600

    Function to count the items collected by an individual
    This functions counts the number of items (correct or individual) collected by an individual.

--- Data_Functions.R
+++ Data_Functions.R
@@ -29,7 +29,6 @@ total_number_of_errors_team <- function(date_errors, teamNum, condition){
                              date_errors$expcondition == condition)
 }
 
-
 # Function to count the total number of errors commited by a player (regarless of if this rule was broken before) ----
 total_number_of_errors_individual <- function(data_errors, teamNum, playernum, condition){
   total_errors_individual <- 0
@@ -48,8 +47,6 @@ re_factor_columns <- function(userData, columnNames){
   }
   return(factorData)
 }
-
-
 
 # Generate team number list in a given data set ----
 list_of_team_numbers <- function(data, team_number_col_name){
@@ -298,6 +295,14 @@ total_distance_traveled_by_team <- function(position_data, experimentalcondition
   }
   
   return(sum(distance_traveled_by_each_player))
+}
+
+# Total items (correct and incorrect) collected by a player in a given session
+total_items_collected_in_session <- function(inventory_data, team_num, player_num, condition_name){
+  inventory_data_filtered <- inventory_data %>%
+    filter(teamnumber == team_num & expcondition == condition_name & playernum == player_num & itemid != -1)
+  
+  return(length(inventory_data_filtered[,1]))
 }
 
 # Check to see if the random numbers in demographics surveys match the random numbers in the post surveys ----
@@ -701,5 +706,17 @@ generate_figures_ind <- function(Data, num_of_players, figure_titles, y_values_i
   
   setwd(previous_wd_location)
 }
+
 #Test ----
 
+total_items_collected_in_session(inventory_table, 10, 1, "B")
+
+team_num <- 10
+player_num <- 1
+condition_letter <- "B"
+
+# The items (individual or team) collected by the individual
+individual_data_inventory_activity <- inventory_table %>%
+  filter(teamnumber == team_num & expcondition == condition_letter & playernum == player_num & itemid != -1)
+
+length(individual_data_inventory_activity[,1])
