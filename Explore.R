f6a3ea109d29d9f8e0fa7154cd04d77070f6128a
Carter <carter.engen@gmail.com>
2019-03-28 10:41:46 -0500

    Collection Rate Additional Changes
    Additional changes from 3/28 to add collection rate individual analysis.

--- Explore.R
+++ Explore.R
@@ -470,9 +470,9 @@ plot_data_ind %>%
 
 view(plot_data_ind)
 
-##CKE looking at collection_rate comparisons
-dependent_response_ind <- "Collection_rate_correct_item_ind"
-y_label_ind <- "Correct Item Collection Rate"
+## CKE looking at collection_rate comparisons
+dependent_response_ind <- "Collection_rate_ind"
+y_label_ind <- "Collection Rate"
 x_label_ind <- "Target"
 title_response_ind <- "Rate Vs. Target"
 
